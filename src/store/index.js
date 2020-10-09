59a791cc007871cd520f7c4f536c768ef3eb26b2
johnson0903 <johnson50701@gmail.com>
2020-10-09 08:46:14 +0800

    將假資料加入store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import expenseData from '@/static/billing/expenseData.json'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    expenseData: expenseData
   },
   mutations: {
   },
